32cf291278b85c90a62378fdd811c45c8a22d160
Dominik Feger <dk.feger@gmail.com>
2025-02-01 11:29:50 +0100

    optimize enhance1

--- revert/enhance1.py
+++ revert/enhance1.py
@@ -3,29 +3,25 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-# Load the image
-image_path = "img.jpeg"
+image_path = "img2.jpeg"
 image = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
-# Check if the image was loaded correctly
 if image is not None:
-    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Apply a sharpening filter to enhance edges
+    # Apply a sharpening and denoise filter to enhance edges
     kernel = np.array([[0, -1, 0],
                        [-1, 5,-1],
                        [0, -1, 0]])
     sharpened = cv2.filter2D(gray, -1, kernel)
-
     denoised = cv2.bilateralFilter(sharpened, d=9, sigmaColor=75, sigmaSpace=75)
 
-# Apply adaptive histogram equalization to improve contrast
+    #apply adaptive histogram equalization to improve contrast
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     equalized = clahe.apply(denoised)
 
-# Upscale again for better visualization
-    upscale_factor = 4  # Further scaling up
+    #upscale again for better visualization
+    upscale_factor = 4 
     enhanced_image = cv2.resize(equalized, None, fx=upscale_factor, fy=upscale_factor, interpolation=cv2.INTER_CUBIC)
 
     # Display the enhanced image
@@ -33,5 +29,6 @@ if image is not None:
     plt.imshow(enhanced_image, cmap='gray')
     plt.axis('off')
     plt.show()
+    cv2.imwrite("enhance1_img2.png", enhanced_image)
 else:
     print("Failed to load image.")
